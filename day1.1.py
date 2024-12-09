60ad25958b485d66586f8e41d35fc1ff214bdcc5
mcneilr2 <robyn55555@gmail.com>
2024-12-08 16:22:07 -0800

    day1 complete

---
+++ day1.1.py
@@ -0,0 +1,14 @@
+file = open("day1.txt", "r")
+string = file.read()
+
+listAll = string.split()
+listA = [int(i) for j, i in enumerate(listAll) if j % 2 != 0]
+listB = [int(i) for j, i in enumerate(listAll) if j % 2 == 0]
+listA.sort()
+listB.sort()
+sum = 0
+for i, x in enumerate(listA):
+    sum += listB.count(x)*x
+
+
+print(sum)
